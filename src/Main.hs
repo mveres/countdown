e04388e2eaaa5123fb5e8aced5cf3e3ff14140e7
Mihai Veres <veres85@yahoo.com>
2014-11-24 11:36:45 -0800

    op & apply

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,12 @@ module Main where
 main :: IO ()
 main = do
     putStrLn "Welcome to the Countdown problem!"
+
+
+data Op = Add | Sub | Mul | Div
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
