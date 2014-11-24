55a5bd52fa2d19221e5925178011fe64470fcf87
Mihai Veres <veres85@yahoo.com>
2014-11-24 13:06:40 -0800

    complete solution

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-main :: IO ()
-main = do
+import System.CPUTime
+import Numeric
+import System.IO
+
+main' :: IO ()
+main' = do
     print $ head $ solutions [1, 3, 7, 10, 25, 50] 765
     print $ subs [1,2]
     print $ interleave 1 [2,3]
@@ -9,7 +13,7 @@ main = do
     print $ choices [1,2,3]
     print $ split [1,2,3]
 
-data Op = Add | Sub | Mul | Div deriving Show
+data Op = Add | Sub | Mul | Div
 
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
@@ -25,8 +29,7 @@ valid Mul x y = x >= y
 valid Div x y = x `mod` y == 0
 
 
-data Expr = Val Int | App Op Expr Expr 
-    deriving Show
+data Expr = Val Int | App Op Expr Expr
 
 eval :: Expr -> [Int]
 eval (Val n) = [n | n > 0]
@@ -79,3 +82,108 @@ solutions                     :: [Int] -> Int -> [Expr]
 solutions ns n                =  [e | ns' <- choices ns
                                     , e   <- exprs ns'
                                     , eval e == [n]]
+
+--Combining generation and evaluation--
+---------------------------------------
+
+type Result                   =  (Expr,Int)
+
+results                       :: [Int] -> [Result]
+results []                    =  []
+results [n]                   =  [(Val n,n) | n > 0]
+results ns                    =  [res | (ls,rs) <- split ns
+                                      , lx      <- results ls
+                                      , ry      <- results rs
+                                      , res     <- combine' lx ry]
+
+combine'                      :: Result -> Result -> [Result]
+combine' (l,x) (r,y)          =  [(App o l r, apply o x y) | o <- ops
+                                                           , valid o x y]
+
+solutions'                    :: [Int] -> Int -> [Expr]
+solutions' ns n               =  [e | ns'   <- choices ns
+                                    , (e,m) <- results ns'
+                                    , m == n]
+
+--Exploiting numeric properties--
+---------------------------------
+
+valid'                        :: Op -> Int -> Int -> Bool
+valid' Add x y                =  x <= y
+valid' Sub x y                =  x > y
+valid' Mul x y                =  x /= 1 && y /= 1 && x <= y
+valid' Div x y                =  y /= 1 && x `mod` y == 0
+
+results'                      :: [Int] -> [Result]
+results' []                   =  []
+results' [n]                  =  [(Val n,n) | n > 0]
+results' ns                   =  [res | (ls,rs) <- split ns
+                                      , lx      <- results' ls
+                                      , ry      <- results' rs
+                                      , res     <- combine'' lx ry]
+
+combine''                     :: Result -> Result -> [Result]
+combine'' (l,x) (r,y)         =  [(App o l r, apply o x y) | o <- ops
+                                                           , valid' o x y]
+
+solutions''                   :: [Int] -> Int -> [Expr]
+solutions'' ns n              =  [e | ns'   <- choices ns
+                                    , (e,m) <- results' ns'
+                                    , m == n]
+
+--Interactive version for testing--
+-----------------------------------
+
+instance Show Op where
+   show Add                   =  "+"
+   show Sub                   =  "-"
+   show Mul                   =  "*"
+   show Div                   =  "/"
+
+instance Show Expr where
+   show (Val n)               =  show n
+   show (App o l r)           =  bracket l ++ show o ++ bracket r
+                                 where
+                                    bracket (Val n) = show n
+                                    bracket e       = "(" ++ show e ++ ")"
+
+showtime                      :: Integer -> String
+showtime t                    =  showFFloat (Just 3)
+                                    (fromIntegral t / (10^12)) " seconds"
+
+display                       :: [Expr] -> IO ()
+display es                    =  do t0 <- getCPUTime
+                                    if null es then
+                                       do t1 <- getCPUTime
+                                          putStr "\nThere are no solutions, verified in "
+                                          putStr (showtime (t1 - t0))
+                                     else
+                                       do t1 <- getCPUTime
+                                          putStr "\nOne possible solution is "
+                                          putStr (show (head es))
+                                          putStr ", found in "
+                                          putStr (showtime (t1 - t0))
+                                          putStr "\n\nPress return to continue searching..."
+                                          getLine
+                                          putStr "\n"
+                                          t2 <- getCPUTime
+                                          if null (tail es) then
+                                             putStr "There are no more solutions"
+                                           else
+                                             do sequence [print e | e <- tail es]
+                                                putStr "\nThere were "
+                                                putStr (show (length es))
+                                                putStr " solutions in total, found in "
+                                                t3 <- getCPUTime
+                                                putStr (showtime ((t1 - t0) + (t3 - t2)))
+                                    putStr ".\n\n"
+
+main                          :: IO ()
+main                          =  do hSetBuffering stdout NoBuffering
+				    putStrLn "\nCOUNTDOWN NUMBERS GAME SOLVER"
+                                    putStrLn "-----------------------------\n"
+                                    putStr "Enter the given numbers : "
+                                    ns <- readLn
+                                    putStr "Enter the target number : "
+                                    n  <- readLn
+                                    display (solutions'' ns n)
