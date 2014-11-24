22e20dee5e25cd84e27eb3890e0ac468355678f5
Mihai Veres <veres85@yahoo.com>
2014-11-24 11:45:50 -0800

    valid, expr and eval

--- src/Main.hs
+++ src/Main.hs
@@ -12,3 +12,17 @@ apply Add x y = x + y
 apply Sub x y = x - y
 apply Mul x y = x * y
 apply Div x y = x `div` y
+
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = x > y
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
+
+data Expr = Val Int | App Op Expr Expr
+
+eval :: Expr -> [Int]
+eval (Val n) = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l, y <- eval r, valid o x y]
